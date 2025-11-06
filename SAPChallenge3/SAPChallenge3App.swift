7516581bf50ec15a4873804a35c570c297b25d9b
tessalje <assetessa77@gmail.com>
2025-11-06 13:08:35 +0800

    Initial Commit

---
+++ SAPChallenge3/SAPChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  SAPChallenge3App.swift
+//  SAPChallenge3
+//
+//  Created by Tessa Lee on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SAPChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SAPChallenge3Tests/SAPChallenge3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SAPChallenge3Tests.swift
+//  SAPChallenge3Tests
+//
+//  Created by Tessa Lee on 6/11/25.
+//
+
+import Testing
+@testable import SAPChallenge3
+
+struct SAPChallenge3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
