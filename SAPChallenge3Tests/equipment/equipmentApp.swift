53c48084d1ecf810b0b6a6e0c06b36818974eb31
Reann <reann.ng@students.sota.edu.sg>
2025-11-07 15:09:26 +0800

    IM SO SICK AND TIRED ARGHHGHG

---
+++ SAPChallenge3Tests/equipment/equipmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  equipmentApp.swift
+//  equipment
+//
+//  Created by .. on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct equipmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomescreenView()
+        }
+    }
+}
