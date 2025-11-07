5269e3ce931b71f9abb6736de3fac1455bf735e7
Reann <reann.ng@students.sota.edu.sg>
2025-11-07 15:17:03 +0800

    as

---
+++ SAPChallenge3Tests/equipmentTests/equipmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  equipmentTests.swift
+//  equipmentTests
+//
+//  Created by .. on 7/11/25.
+//
+
+import Testing
+
+struct equipmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
